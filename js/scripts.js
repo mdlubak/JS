df6d8144aa3e90e8d5b7a4921d7b529cee55f94c
Martyna <mdlubak@gmail.com>
2016-12-29 00:22:58 +0000

    Initial

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-//8.5
-var
-	a = 2;
- 	b = 3;
-  	value = (a*a) + (2 * a * b) - (b*b);
+//8.6
+function getTriangleArea(a, h) {
+  if (a, h <= 0) {
+    console.log('Nieprawidłowe dane') 
+  } else {
+    return (a*h/2) 
+  }
+}
 
-console.log(value);
+var 
+  triangle1Area = getTriangleArea(10, 15);
+  triangle2Area = getTriangleArea(9, 14); 
+  triangle3Area = getTriangleArea(8, 13);
 
-if (value > 0) {
-  	console.log('Wynik dodatni')
-}
-else if (value < 0) { 
-  	console.log('Wynik ujemny')
-}
-else if (value == 0) {
-  	console.log('Zero')
-}
+console.log (triangle1Area);
+console.log (triangle2Area);
+console.log (triangle3Area);
