b20d734ed6dddc904844387461a6b66aafbdf116
Martyna <mdlubak@gmail.com>
2016-12-29 00:05:34 +0000

    Initial

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//8.4
+var 
+	a = prompt('Wysokosc trojkata');
+	h = prompt('Podstawa trojkata');
+	triangleArea = a*h/2 
+
+alert('Pole trójkata wynosi: ' +  triangleArea);
+console.log('Pole trójkata o podstawie a: ' + a + ' i wysokosci h: ' + h + ' wynosi: ' + triangleArea);
