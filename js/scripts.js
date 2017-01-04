ee3cc92762f67ef17424419b31316f7643199101
Martyna <mdlubak@gmail.com>
2017-01-04 23:07:17 +0000

    Initial 8.11

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var list = document.getElementById("lista");
+var add = document.getElementById("addElem");
+
+add.addEventListener("click", function() {
+ 
+  var listElemByTag = document.getElementsByTagName("li");
+
+  list.innerHTML += "<li>Item " + listElemByTag.length + "</li>";
+});
