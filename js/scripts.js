760630a3585213581d0938a5f613687bb584ee2d
mdlubak <mdlubak@gmail.com>
2017-01-04 23:46:57 +0000

    Fix spaces, semicolons, else if

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 //8.5
-var
-	a = 2;
- 	b = 3;
-  value = (a*a) + (2 * a * b) - (b*b);
+var a = 2;
+var b = 3;	
+var value = (a * a) + (2 * a * b) - (b * b);
 
 console.log(value);
 
 if (value > 0) {
-  	console.log('Wynik dodatni')
-}
-else if (value < 0) { 
-  	console.log('Wynik ujemny')
-}
-else if (value == 0) {
-  	console.log('Zero')
+	console.log('Wynik dodatni');
+} else if (value < 0) { 
+  	console.log('Wynik ujemny');
+} else {
+  	console.log('Liczba jest równa zero');
 }
