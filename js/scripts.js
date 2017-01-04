ad502d7908b043e685b9312bec948324f7faea5c
mdlubak <mdlubak@gmail.com>
2017-01-04 23:19:30 +0000

    Add spaces and semicolon

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function drawTree(height) {
-  for (var k=1; k <= height; k++) {
+  for (var k = 1; k <= height; k++) {
     var star = "";
-    for (var x=1; x <= height-k; x++) {
-     star += " "
+    for (var x = 1; x <= height - k; x++) {
+     star += " ";
     }
-    for (var n=1 ; n <= k*2-1; n++) {
+    for (var n = 1; n <= k * 2 - 1; n++) {
      star += "*";
     }
     console.log(star);
