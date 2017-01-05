969c7036be92242f5a08d2a5a274f7c42c0f0b3f
Martyna <mdlubak@gmail.com>
2017-01-05 16:11:10 +0000

    Correct spacing and var

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 //8.4
-var 
-	a = prompt('Wysokosc trojkata');
-	h = prompt('Podstawa trojkata');
-	triangleArea = a*h/2 
+var	a = prompt('Wysokosc trojkata');
+var	h = prompt('Podstawa trojkata');
+var	triangleArea = a * h / 2;  
 
 alert('Pole trójkata wynosi: ' +  triangleArea);
 console.log('Pole trójkata o podstawie a: ' + a + ' i wysokosci h: ' + h + ' wynosi: ' + triangleArea);
