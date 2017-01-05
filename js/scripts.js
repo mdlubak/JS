01498a668da8e3c5a5722fc0b2f076483afd5db4
Martyna <mdlubak@gmail.com>
2017-01-05 23:37:21 +0000

    Replace papugi with split() method

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 //8.8
-var
-	text = ('Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.');
-	textElephant = ('Zielone słonie');
-	textElephantUpperCased = textElephant.toUpperCase();
-	textReplaced = text.replace('Papugi', textElephantUpperCased);
-	textReplacedHalf = textReplaced.substr(0, textReplaced.length/2);
-	
+var text = ('Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.');
+var textElephant = ('Zielone słonie');
+var textElephantUpperCased = textElephant.toUpperCase();
+var textFirstWord = text.split(" ", 1);
+var textReplaced = text.replace(textFirstWord, textElephantUpperCased);
+var textReplacedHalf = textReplaced.substr(0, textReplaced.length / 2);
+
+console.log(textFirstWord);	
 console.log(textReplaced);
 console.log(textReplacedHalf);
 
