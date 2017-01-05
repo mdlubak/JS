82d66b75fb447cd0bb09a1d30b7e2561b601bbca
Martyna <mdlubak@gmail.com>
2017-01-05 23:09:15 +0000

    Fix if statement, var, semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 //8.7
-var 
-	womensNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-	mensNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-	allNames = womensNames.concat(mensNames);
-	newName = prompt('Wpisz imię: ');
-	indexNewName = allNames.indexOf(newName);
+var womensNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var mensNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = womensNames.concat(mensNames);
+var newName = prompt('Wpisz imię: ');
 
 console.log(allNames);
 
-if (indexNewName == -1) {
-	console.log('Imię może zostać dodane')
-	allNames.push(newName)
+if (allNames.indexOf(newName) === -1) {
+	console.log('Imię może zostać dodane');
+	allNames.push(newName);
+  	console.log(allNames);
 }
 else {
-	console.log('Imię już występuje w tabeli')
+	console.log('Imię już występuje w tabeli');
 }
-
-console.log(allNames);
