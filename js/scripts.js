42f598e8e1f8550acbf049350e47022277d7f01f
Martyna <mdlubak@gmail.com>
2017-01-06 00:05:32 +0000

    Fix tabs

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ var list = document.getElementById("lista");
 var add = document.getElementById("addElem");
 
 add.addEventListener("click", function() {
- 
-  var listElemByTag = document.getElementsByTagName("li");
-
-  list.innerHTML += "<li>Item " + listElemByTag.length + "</li>";
+	var listElemByTag = document.getElementsByTagName("li");
+	list.innerHTML += "<li>Item " + listElemByTag.length + "</li>";
 });
